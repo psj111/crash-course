41b9b39d03a2437d63195838900a667048ae7437
Pratik Jagrut <pjagrut@redhat.com>
2018-08-24 17:19:13 +0530

    Tests: Adding pratik to the attendees list
    Signed-off-by: Pratik Jagrut <pjagrut@redhat.com>

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 print("Welcome to the crash course!")
 
-attendees = ["fidencio", "Madhuri", "Dan","Steeve"]
+attendees = ["fidencio", "Madhuri", "Dan", "Steeve", "pratik"]
 
 for attendee in attendees:
     print("- %s" % (attendee))
